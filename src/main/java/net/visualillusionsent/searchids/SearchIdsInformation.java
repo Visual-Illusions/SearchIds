a93b3750dcae49f3ca2ac576ad777282054f8580
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-06-26 13:21:41 -0500

    fix dependency on other mods where we should be independant

--- src/main/java/net/visualillusionsent/searchids/SearchIdsInformation.java
+++ src/main/java/net/visualillusionsent/searchids/SearchIdsInformation.java
@@ -1,10 +1,25 @@
+/*
+ * This file is part of SearchIds.
+ *
+ * Copyright © 2012-2013 Visual Illusions Entertainment
+ *
+ * SearchIds is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * SearchIds is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with SearchIds.
+ * If not, see http://www.gnu.org/licenses/gpl.html.
+ */
 package net.visualillusionsent.searchids;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import net.canarymod.chat.Colors;
-import net.canarymod.chat.TextFormat;
 import net.visualillusionsent.utils.StringUtils;
 
 public final class SearchIdsInformation {
@@ -13,13 +28,13 @@ public final class SearchIdsInformation {
 
     public SearchIdsInformation(SearchIds plugin) {
         List<String> pre = new ArrayList<String>();
-        pre.add(center(Colors.CYAN + "--- " + Colors.LIGHT_GREEN + plugin.getName() + Colors.ORANGE + " v" + plugin.getRawVersion() + Colors.CYAN + " ---"));
+        pre.add(center("§B--- §A" + plugin.getName() + "§6 v" + plugin.getRawVersion() + "§B ---"));
         pre.add("$VERSION_CHECK$");
-        pre.add(Colors.CYAN + "Build: " + Colors.LIGHT_GREEN + plugin.getBuildNumber());
-        pre.add(Colors.CYAN + "Built: " + Colors.LIGHT_GREEN + plugin.getBuildTime());
-        pre.add(Colors.CYAN + "Developer: " + Colors.LIGHT_GREEN + "DarkDiplomat");
-        pre.add(Colors.CYAN + "Website: " + Colors.LIGHT_GREEN + "http://wiki.visualillusionsent.net/" + plugin.getName());
-        pre.add(Colors.CYAN + "Issues: " + Colors.LIGHT_GREEN + "https://github.com/Visual-Illusions/" + plugin.getName() + "/issues");
+        pre.add("§BBuild: §A" + plugin.getBuildNumber());
+        pre.add("§BBuilt: §A" + plugin.getBuildTime());
+        pre.add("§BDeveloper: §ADarkDiplomat");
+        pre.add("§BWebsite: §Ahttp://wiki.visualillusionsent.net/" + plugin.getName());
+        pre.add("§BIssues: §Ahttps://github.com/Visual-Illusions/" + plugin.getName() + "/issues");
 
         // Next line should always remain at the end of the About
         pre.add(center("§aCopyright © 2012-2013 §2Visual §6I§9l§bl§4u§as§2i§5o§en§7s §2Entertainment"));
@@ -31,7 +46,11 @@ public final class SearchIdsInformation {
     }
 
     public final String center(String toCenter) {
-        String strColorless = TextFormat.removeFormatting(toCenter);
+        String strColorless = removeFormatting(toCenter);
         return StringUtils.padCharLeft(toCenter, (int) (Math.floor(63 - strColorless.length()) / 2), ' ');
+    }
+
+    private final String removeFormatting(String str) {
+        return str.replace("\u00A7[A-FK-ORa-fk-or0-9]", "");
     }
 }
